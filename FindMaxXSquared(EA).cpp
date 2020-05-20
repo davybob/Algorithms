1a1ada9c5908af37946195d2ef8638083a3987cb
davy <davybob123@gmail.com>
2020-05-21 00:52:33 +0100

    Clean up + oop
    NEED TO ADD OPTION TO ADD PROBABILITY OF MATES/ MUTATIONS RATES &
    TERMINATION CONDITION

--- FindMaxXSquared(EA).cpp
+++ FindMaxXSquared(EA).cpp
@@ -4,15 +4,54 @@
 #include <bitset>
 #include <cmath>
 
-int Fitness(std::bitset<5> Population){
-  return Population.to_ulong()*Population.to_ulong();
+void Fitness(std::bitset<5> *Population, float* fitness, int& sum, float& avg, int& max){
+  for (int i = 0; i < 4; i++){
+    fitness[i] = Population[i].to_ulong()*Population[i].to_ulong();
+    sum += fitness[i];
+    if (fitness[i] > max ) max = fitness[i];
+  }
+  avg = sum/4;
 }
-float ProbCont(float fitness, float sum){
-  return fitness/sum;
-}   
-float expected(int fitness, int avg){
-  return fitness/avg;
+
+void Counts(float *ProbCount, float *ExpectedCount, float *fitness, float sum, float avg){
+    for (int i = 0; i < 4; i++){
+    ProbCount[i] = fitness[i]/sum;
+    ExpectedCount[i] = fitness[i]/avg;
+  }
 }
+
+void ChooseOffspring(float *probCount, int *actualCount, std::default_random_engine& generator){
+    int i = 0;
+    int total = 0;
+    while(total < 4){
+    if (i==4)
+      i = 0;
+    std::bernoulli_distribution tempdist(probCount[i]);
+    bool result = tempdist(generator);
+    if (result){
+      actualCount[i] += 1;
+      total += 1;
+    }
+    i += 1;
+    }
+}
+
+void ElimOldOffspring(int *actualCount,std::bitset<5> *Population){
+  auto tempcount = actualCount;
+  for(int i = 0; i < 4; i++){
+    if (tempcount[i] > 1){
+      for(int j = 0; j < 4; j++){
+        if (tempcount[i] < 2)
+          break;
+        if (tempcount[j] == 0){
+          tempcount[j] += 1;
+          Population[j] = Population[i];
+          tempcount[i] -= 1;}
+      }
+    }
+  }
+}
+
 void Mate(std::bitset<5> &offspring1,std::bitset<5> &offspring2, std::default_random_engine generator){
   std::bernoulli_distribution tempdist(0.2);
   bool result = tempdist(generator);
@@ -29,11 +68,32 @@ void Mate(std::bitset<5> &offspring1,std::bitset<5> &offspring2, std::default_ra
 }
 
 void Mutate(std::bitset<5> &Population, std::default_random_engine& generator){
-  std::bernoulli_distribution tempdist(0.02);
+  std::bernoulli_distribution tempdist(0.2);
   bool result = tempdist(generator);
   int i = Population.to_ulong()%5;
   if (result)
     Population.set(i,tempdist(generator));
+}
+
+void GenerateNewOffspring(std::bitset<5> *Population, std::default_random_engine &generator){
+  int i = 0;
+  int j = 0;
+  while(true){
+    if (i == 4 )
+      i = 0;
+    std::bernoulli_distribution tempdist(0.25);
+
+    bool result = tempdist(generator);
+    if (result && i != j){
+      Mate(Population[j], Population[i], generator);
+      Mutate(Population[j], generator);
+      if(i != 1 && i!= 2){Mate(Population[1], Population[2], generator); Mutate(Population[1],generator);Mutate(Population[2],generator);}
+      else if(i != 1 && i!=3){Mate(Population[1], Population[3], generator);Mutate(Population[1],generator);Mutate(Population[2],generator);}
+      else if(i != 2 && i!=3){Mate(Population[2], Population[3], generator);Mutate(Population[1],generator);Mutate(Population[2], generator);}
+      break;
+    }
+      i += 1;
+  }
 }
 
 void Table(std::bitset<5> Population[4], float *fitness, float *probcont, float *expected, int *actual, int sum, float avg, int max){
@@ -50,7 +110,7 @@ void Table(std::bitset<5> Population[4], float *fitness, float *probcont, float 
   }
   std::cout << "Sum" << "\t \t " << sum <<"\n";
   std::cout << "Avg" << "\t \t " << avg <<"\n";
-  std::cout << "Max" << "\t \t " << max <<"\n";
+  std::cout << "Max" << "\t \t " << max <<"\n \n";
 
 }
 
@@ -74,7 +134,7 @@ int main() {
   }
 
 // LOOP FOR EACH ITERATION
-for(int z = 0; z < 20; z++){
+for(int z = 0; z < 9; z++){
   float PopulationFitness[4];
   int sum = 0;
   float avg = 0;
@@ -82,70 +142,23 @@ for(int z = 0; z < 20; z++){
   float probcont[4]= {0,0,0,0};
   float expectedCount[4]= {0,0,0,0};
   int actualCount[4] = {0,0,0,0};
-  // Get current population fitness
-  for (int i = 0; i < 4; i++){
-    PopulationFitness[i] = Fitness(Population[i]);
-    sum += PopulationFitness[i];
-  }
-  avg = sum/4;
-  max = PopulationFitness[0];
+  // Get current population fitness, max, sum, avg
+  Fitness(Population,PopulationFitness,sum,avg,max);
+
   // Get current population probability of offspring
-  // Find max
   // Find Expected count of each offspring
-  for (int i = 0; i < 4; i++){
-    probcont[i] = ProbCont(PopulationFitness[i], sum);
-    expectedCount[i] = PopulationFitness[i]/avg;
-    if (PopulationFitness[i] > max || (i < 1 && PopulationFitness[i] > PopulationFitness[i-1]))
-      max = PopulationFitness[i];
-  }
+  Counts(probcont, expectedCount,PopulationFitness,sum,avg);
+  //Choose offspring using bernoulli and probcont of each offspring, loop until we count population size of offspring
+  ChooseOffspring(probcont, actualCount, generator);
+
   int total = 0;
   int i = 0;
-  // Choose offspring using bernoulli and probcont of each offspring, keep looping until we count population size of offspring
-  while(total < 4){
-    if (i==4)
-      i = 0;
-    std::bernoulli_distribution tempdist(probcont[i]);
-    bool result = tempdist(generator);
-    if (result){
-      actualCount[i] += 1;
-      total += 1;
-    }
-    i += 1;
-  }
+
   // Eliminate unchosen offspring and copy chosen offspring to keep population constant
-  auto tempcount = actualCount;
-  for(int i = 0; i < 4; i++){
-    if (tempcount[i] > 1){
-      for(int j = 0; j < 4; j++){
-        if (tempcount[i] < 2)
-          break;
-        if (tempcount[j] == 0){
-          tempcount[j] += 1;
-          Population[j] = Population[i];
-          tempcount[i] -= 1;}
-      }
-    }
-  }
+  ElimOldOffspring(actualCount, Population);
+
   // Pair population offspring together for mating (Choose point to crossover then crossover)
-  i = 0;
-  int j = 0;
-  while(true){
-    if (i == 4 )
-      i = 0;
-    std::bernoulli_distribution tempdist(0.25);
-
-    bool result = tempdist(generator);
-    if (result && i != j){
-      Mate(Population[j], Population[i], generator);
-      Mutate(Population[j], generator);
-      if(i != 1 && i!= 2){Mate(Population[1], Population[2], generator); Mutate(Population[1],generator);Mutate(Population[2],generator);}
-      else if(i != 1 && i!=3){Mate(Population[1], Population[3], generator);Mutate(Population[1],generator);Mutate(Population[2],generator);}
-      else if(i != 2 && i!=3){Mate(Population[2], Population[3], generator);Mutate(Population[1],generator);Mutate(Population[2], generator);}
-      break;
-    }
-      i += 1;
-  }
-
+  GenerateNewOffspring(Population, generator);
 
   Table(Population, PopulationFitness, probcont, expectedCount, actualCount, sum, avg, max);
 }
